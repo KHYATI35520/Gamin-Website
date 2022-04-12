ebc972bf880c31e86a57466cf92181b05db333a4
KHYATI35520 <88891086+KHYATI35520@users.noreply.github.com>
2022-04-12 13:53:58 +0530

    Update dragon.js.js

--- dragon.js.js
+++ dragon.js.js
@@ -1,8 +1,8 @@
 score = 0;
 cross = true;
 
-audio = new Audio('../MEDIA/music.mp3');
-audiogo = new Audio('../MEDIA/gameover.mp3');
+audio = new Audio('music.mp3');
+audiogo = new Audio('gameover.mp3');
 setTimeout(() => {
     audio.play()
 }, 1000);
